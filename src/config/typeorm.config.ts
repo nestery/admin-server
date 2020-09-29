f2acb5a30ac9760170b8df7f052eba13f47cc5fb
rsafin <favoritt>
2020-09-29 20:14:17 +0300

    updated ip

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,7 +2,7 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
     type: 'postgres',
-    host: 'localhost',
+    host: '192.168.0.16',
     port: 5432,
     username: 'postgres',
     password: 'postgres',
